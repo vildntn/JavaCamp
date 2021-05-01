80e745bebaa7b023b743135032d6c371747aa3ec
Lenovo <Lenovo@DESKTOP-TCNT505>
2021-05-01 16:17:38 +0300

    Deneme

--- javaCampThirdHw/src/javaCampThirdHw/Instructor.java
+++ javaCampThirdHw/src/javaCampThirdHw/Instructor.java
@@ -8,7 +8,6 @@ public class Instructor extends User {
 		
 	}
 
-	
 	public Instructor(int id, String firstName, String lastName, String email,String[] givenCourse) {
 		super(id,firstName,lastName,email);
 		this.givenCourse = givenCourse;
