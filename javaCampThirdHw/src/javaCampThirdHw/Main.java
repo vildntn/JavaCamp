03c9dc6e7e35342da9311d2ef6d8ba49ba00e2e6
Lenovo <Lenovo@DESKTOP-TCNT505>
2021-05-01 16:32:39 +0300

    Deneme

--- javaCampThirdHw/src/javaCampThirdHw/Main.java
+++ javaCampThirdHw/src/javaCampThirdHw/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		String[] courses= {"C#&Angular","Java&React"};
 		Student student1=new Student(1, "Adam","Smith","adam@adam.com","Java&React");
-
 		Instructor instructor1=new Instructor();
 		instructor1.setFirstName("Engin");
 		instructor1.setEmail("engin@engin.com");
