e87f8c67949616f37cd04e34b0b087a5b9d6a559
Lenovo <Lenovo@DESKTOP-TCNT505>
2021-05-03 22:46:45 +0300

    New commit

--- GameProjectDemo/src/Concreate/SalesManager.java
+++ GameProjectDemo/src/Concreate/SalesManager.java
@@ -9,7 +9,7 @@ public class SalesManager implements SalesService{
 
 	@Override
 	public void sell(Customer customer, Campaign campaign, Game game) {
-		System.out.println(game.getGameName()+ "The game was purchased by: "+customer.getFirstName()+" with this campaign: "+ campaign.getCampaignName());
+		System.out.println(game.getGameName()+ "The game was purchased by: "+customer.getFirstName()+" with this campaign: "+ campaign.getCampaignName()+" Price: " +(game.getPrice()-(game.getPrice()*campaign.getDiscount()/100)));
 		
 	}
 
---
+++ interfaceAbstractDemo/src/interfaceAbstractDemo/Customer.java
@@ -0,0 +1,5 @@
+package interfaceAbstractDemo;
+
+public class Customer {
+
+}
