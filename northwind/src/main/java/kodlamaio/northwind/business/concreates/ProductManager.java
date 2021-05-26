bf3c5951381f86284cf9ec50a6f200e873196f76
Lenovo <Lenovo@DESKTOP-TCNT505>
2021-05-26 16:05:53 +0300

    26.05.2021

---
+++ northwind/src/main/java/kodlamaio/northwind/business/concreates/ProductManager.java
@@ -0,0 +1,37 @@
+package kodlamaio.northwind.business.concreates;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
+import kodlamaio.northwind.entities.concreates.Product;
+
+@Service
+public class ProductManager implements ProductService{
+
+	private ProductDao productDao;
+	
+	@Autowired
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
+
+	@Override
+	public DataResult<List<Product>> getAll() {
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(), "Data listelendi");
+	}
+
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessDataResult<Product>("ürün eklendi");
+	}
+
+}
